37a04966763581086160a5875b239288416303d6
OctopusLian <zoctopus@qq.com>
2023-08-29 19:09:56 +0800

    perf: 让请求更快找到目标函数

--- framework/core.go
+++ framework/core.go
@@ -1,36 +1,94 @@
 package framework
 
 import (
-	"log"
 	"net/http"
+	"strings"
 )
 
 // 框架核心结构
 type Core struct {
-	router map[string]ControllerHandler
+	router map[string]map[string]ControllerHandler // 二级map
 }
 
 // 初始化框架核心结构
 func NewCore() *Core {
-	return &Core{}
+	// 定义二级map
+	getRouter := map[string]ControllerHandler{}
+	postRouter := map[string]ControllerHandler{}
+	putRouter := map[string]ControllerHandler{}
+	deleteRouter := map[string]ControllerHandler{}
+
+	// 将二级map写入一级map
+	router := map[string]map[string]ControllerHandler{}
+	router["GET"] = getRouter
+	router["POST"] = postRouter
+	router["PUT"] = putRouter
+	router["DELETE"] = deleteRouter
+
+	return &Core{router: router}
 }
 
+// 对应 Method = Get
 func (c *Core) Get(url string, handler ControllerHandler) {
-	c.router[url] = handler
+	uppeUrl := strings.ToUpper(url) // 大写转换
+	c.router["GET"][uppeUrl] = handler
 }
 
-// 框架核心架构实现Handler接口
+// 对应 Method = POST
+func (c *Core) Post(url string, handler ControllerHandler) {
+	uppeUrl := strings.ToUpper(url)
+	c.router["POST"][uppeUrl] = handler
+}
+
+// 对应 Method = PUT
+func (c *Core) Put(url string, handler ControllerHandler) {
+	uppeUrl := strings.ToUpper(url)
+	c.router["PUT"][uppeUrl] = handler
+}
+
+// 对应 Method = DELETE
+func (c *Core) Delete(url string, handler ControllerHandler) {
+	uppeUrl := strings.ToUpper(url)
+	c.router["DELETE"][uppeUrl] = handler
+}
+
+func (c *Core) Group(prefix string) IGroup {
+	return NewGroup(c, prefix)
+}
+
+// 匹配路由，如果没有匹配到，返回nil
+func (c *Core) FindRouteByRequest(request *http.Request) ControllerHandler {
+	// uri 和 method 全部转换为大写，保证大小写不敏感
+	uri := request.URL.Path
+	method := request.Method
+	upperMethod := strings.ToUpper(method)
+	upperUri := strings.ToUpper(uri)
+
+	// 查找第一层map
+	if methodHandlers, ok := c.router[upperMethod]; ok {
+		// 查找第二层map
+		if handler, ok := methodHandlers[upperUri]; ok {
+			return handler
+		}
+	}
+	return nil
+}
+
 func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	log.Println("core.serveHTTP")
+	// 封装自定义context
 	ctx := NewContext(request, response)
 
-	// 一个简单的路由选择器，这里直接写死为测试路由foo
-	router := c.router["foo"]
+	// 寻找路由
+	router := c.FindRouteByRequest(request)
 	if router == nil {
+		// 如果没有找到，这里打印日志
+		ctx.Json(404, "not found")
 		return
 	}
-	log.Println("core.router")
 
-	router(ctx)
-
+	// 调用路由函数，如果返回err 代表存在内部错误，返回500状态码
+	if err := router(ctx); err != nil {
+		ctx.Json(500, "inner error")
+		return
+	}
 }
