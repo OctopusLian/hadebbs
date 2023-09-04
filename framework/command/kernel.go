446b7420277efb7d84d0cc25d876e802f42297b6
OctopusLian <zoctopus@qq.com>
2023-09-04 15:22:38 +0800

    feat(framework): 环境变量服务接口实现

--- framework/command/kernel.go
+++ framework/command/kernel.go
@@ -4,7 +4,12 @@ import "hadebbs/framework/cobra"
 
 // AddKernelCommands will add all command/* to root command
 func AddKernelCommands(root *cobra.Command) {
-	root.AddCommand(DemoCommand)
+	//root.AddCommand(DemoCommand)
 
+	// env
+	root.AddCommand(initEnvCommand())
+	// app
 	root.AddCommand(initAppCommand())
+	// cron
+	root.AddCommand(initCronCommand())
 }
