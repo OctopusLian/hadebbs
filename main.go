854c10facbffbd60b4029507991976246460bfed
OctopusLian <zoctopus@qq.com>
2023-09-04 16:13:22 +0800

    feat(framework): 实现日志服务

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
 	"hadebbs/app/http"
 	"hadebbs/framework"
 	"hadebbs/framework/provider/app"
+	"hadebbs/framework/provider/config"
+	"hadebbs/framework/provider/distributed"
+	"hadebbs/framework/provider/env"
+	"hadebbs/framework/provider/id"
 	"hadebbs/framework/provider/kernel"
+	"hadebbs/framework/provider/log"
+	"hadebbs/framework/provider/trace"
 )
 
 func main() {
@@ -14,6 +20,12 @@ func main() {
 	// 绑定App服务提供者
 	container.Bind(&app.HadeAppProvider{})
 	// 后续初始化需要绑定的服务提供者...
+	container.Bind(&env.HadeEnvProvider{})
+	container.Bind(&distributed.LocalDistributedProvider{})
+	container.Bind(&config.HadeConfigProvider{})
+	container.Bind(&id.HadeIDProvider{})
+	container.Bind(&trace.HadeTraceProvider{})
+	container.Bind(&log.HadeLogServiceProvider{})
 
 	// 将HTTP引擎初始化,并且作为服务提供者绑定到服务容器中
 	if engine, err := http.NewHttpEngine(); err == nil {
